f687d81b2d0b9ab89de4a4d6a62eb2038163008f
Rahim6677 <starter5577@gmail.com>
2026-04-15 20:26:34 +0600

    Revert "Deleted unwanted files"
    This reverts commit 3a66ede9e8d5adf0cb707489bea55dd55b8a38e3.

---
+++ f.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  long long n;
+  cin >> n;
+  if (n % 2 == 0)
+    cout << "REDOY" << endl;
+  else
+    cout << "MARUF" << endl;
+  return 0;
+}
